77458e7013dca4ac7d44d30bb1c309e0f40dfbe9
Adrian Carolli <adriancarolli@Adrians-MacBook-Air.local>
2018-02-07 17:05:05 -0500

    test send money to winner

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -84,4 +84,25 @@ describe('Lottery Contract', () => {
       assert(err);
     }
   });
+  
+  it('sends money to the winner and resets the players array', async() => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('2', 'ether')
+    });
+    
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+    
+    await lottery.methods.pickWinner().send({ from: accounts[0] });
+    
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+    
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+    
+    const difference = finalBalance - initialBalance;
+    assert(difference > web3.utils.toWei('1.8', 'ether'));
+    assert.equal(0, players.length);
+  });
 });
